c5930e48d800870c7486cf13911c94c59bc65e17
Matteo <74088595+rainnfx@users.noreply.github.com>
2025-08-30 16:19:03 -0700

    Add avg.py to calculate min, max, and average

---
+++ unit4/avg.py
@@ -0,0 +1,11 @@
+nums = [23, 45, 67, 8, 6, 90]
+
+minimum = min(nums)
+maximum = max(nums)
+avg = sum(nums)
+
+avg = round(avg, 1)
+
+print(f"Min: {minimum}")
+print(f"Max: {maximum}")
+print(f"Avg: {avg}")
